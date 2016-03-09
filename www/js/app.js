e7963edb99899686e876212a12f497432cf39c48
ankush1990 <ank_ankush@yahoo.co.in>
2016-03-09 20:53:52 +0530

    hkjhjk

--- www/js/app.js
+++ www/js/app.js
@@ -166,6 +166,51 @@ angular.module('ionicApp', ['ionic','ngCordova','starter.controllers'])
 	};
 })
 
+.directive('map', function() {
+	return {
+		restrict: 'E',
+		scope: {
+		  onCreate: '&'
+		},
+		link: function ($scope, $element, $attr) {
+		  function initialize() {
+			var myLatLng = {lat: 27.9769145, lng: -82.5590481};
+			var mapOptions = {
+			  center: new google.maps.LatLng(27.9769145, -82.5590481),
+			  zoom: 16,
+			  mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			var map = new google.maps.Map($element[0], mapOptions);
+			var marker = new google.maps.Marker({
+				position: myLatLng,
+				map: map,
+				label: "A",
+				content:"Hello World!"
+			});
+			var infowindow = new google.maps.InfoWindow({
+			  content:"5424 Ginger Cove Dr"
+			});
+			infowindow.open(map,marker);
+			
+			$scope.onCreate({map: map});
+	
+			// Stop the side bar from dragging when mousedown/tapdown on the map
+			google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
+			  e.preventDefault();
+			  return false;
+			});
+		  }
+	
+		  if (document.readyState === "complete") {
+			initialize();
+		  } else {
+			google.maps.event.addDomListener(window, 'load', initialize);
+		  }
+		}
+  	}
+})
+
+
 .service('popupService', function($rootScope,$ionicPopup){
 	this.popup = function(text){
 		$ionicPopup.show({
--- www/js/controller.js
+++ www/js/controller.js
@@ -185,8 +185,30 @@ angular.module('starter.controllers', [])
   	};
 })
 
-.controller('settingCTRL', function($scope) {
+.controller('settingCTRL', function($scope, $ionicLoading) {
+	$scope.mapCreated = function(map) {
+		$scope.map = map;
+	};
 	
+	$scope.centerOnMe = function () {
+		console.log("Centering");
+		if (!$scope.map) {
+		  return;
+		}
+	
+		$scope.loading = $ionicLoading.show({
+		  content: 'Getting current location...',
+		  showBackdrop: false
+		});
+	
+		navigator.geolocation.getCurrentPosition(function (pos) {
+		  console.log('Got pos', pos);
+		  $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+		  $scope.loading.hide();
+		}, function (error) {
+		  alert('Unable to get location: ' + error.message);
+		});
+	};
 })
 
 .controller('scheduleCTRL', function($scope,$rootScope,$ionicPopup,$ionicModal,popupService,$stateParams) {
